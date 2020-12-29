b793043536a244cc2a0e8e71f3e67dc3eec3b251
edwardokane <eddieokane72@gmail.com>
2020-12-29 11:12:52 +0300

    first commit

---
+++ cms/admin/include/header.php
@@ -0,0 +1,32 @@
+<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+					<i class="icon-reorder shaded"></i>
+				</a>
+
+			  	<a class="brand" href="index.html">
+			  		Stanbic Bank CMS | Admin
+			  	</a><img src="file:///E|/MSc.CSC/Sem 2/Computer System Security/Assignments/System/complaint management system/4.png" width="229" height="58" alt=""/>
+
+				<div class="nav-collapse collapse navbar-inverse-collapse">
+					<ul class="nav pull-right">
+						<li><a href="#">
+							Admin
+						</a></li>
+						<li class="nav-user dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+								<img src="images/user.png" class="nav-avatar" />
+								<b class="caret"></b>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="change-password.php">Change Password</a></li>
+								<li class="divider"></li>
+								<li><a href="logout.php">Logout</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.nav-collapse -->
+			</div>
+		</div><!-- /navbar-inner -->
+	</div><!-- /navbar -->
---
+++ cms/users/includes/header.php
@@ -0,0 +1,13 @@
+ <header class="header black-bg">
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            <!--logo start-->
+            <a href="dashboard.php" class="logo"><b>Stanbic Bank Complaint Management System</b></a>
+            <img src="file:///E|/MSc.CSC/Sem 2/Computer System Security/Assignments/System/complaint management system/2.jpg" width="245" height="98" alt=""/>
+            <div class="top-menu">
+            	<ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="logout.php">Logout</a></li>
+           	  </ul>
+  </div>
+        </header>
